effd77b2608af91baffb818713a305f135a0b267
🐰🍪 <student@students-MacBook-Pro.local>
2018-03-27 22:50:46 -0700

    XandO

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Author:
+//Author: Rashed Alnuaimi
 #include <iostream>
 
 using namespace std;
@@ -40,25 +40,36 @@ int main()
     {
       cout<<"Values must be between 0 and 2.\n";
     }
-    //TODO: Place the piece and swap turns
-    //Outline
-    //1. When neither of the (above) if nor else if statements are true, then...
-    //1-A.  Assign the board location of row and column to the value of turn
-    //1-B.  When turn has the value 'X', then...
-    //1-B-1.  turn should be assigned the value 'O'
-    //1-C.  In any other case, then...
-    //1-C-1.  turn should be assigned the value 'X'
-    
+    else
+    {
+       board [row][column] = turn;
+	
+	
+if (turn == X)
+{
+    turn=O;
+}
+else
+{
+turn = X;
+}
+}    
+  
     cout<<"\nBOARD\n-----\n";
-    //TODO: Print the current board
-    //Outline
-    //1. Traverse through each row, calling the current row r
-    //1-A. Traverse through each column in row r, calling the current column c
-    //1-A-1.  Display the value of the board at location of r and c
-    //1-A-2. Display a space
-    //1-B. Display an newline to move to the next row of the board
-    
-  }while( playing );
+for ( int i = 0 ; i <ROWS; i++ )
+{
+	for (int w =0; w<COLUMNS; w++)
+	{
+	cout<<board [i][w]<<" ";
+	}
+	cout<<endl;
+}
+
+
+
+
+
+ }while( playing );
 
   cout<<"Goodbye!\n";
 
