eb01bd84bf37c7d5e6da385a0e51527727ad9d83
🐰🍪 <student@students-MacBook-Pro.local>
2018-04-05 22:27:56 -0700

    Done with Lab

--- main.cpp
+++ main.cpp
@@ -22,7 +22,8 @@ int main()
   int row;
   int column;
   bool playing = true; //Continue playing by default
-
+  char winner = ' ';
+  bool won = false;
   do
   {
     cout<<turn<<"'s turn.\n";
@@ -42,36 +43,56 @@ int main()
     }
     else
     {
-       board [row][column] = turn;
-	
-	
-if (turn == X)
-{
-    turn=O;
-}
-else
-{
-turn = X;
-}
-}    
+     board[row][column]=turn;
+      if(turn==X)
+        {
+         turn=O;
+        } 
+      else
+        {
+        turn=X;
+        }
+    }
+        cout<<"\nBOARD\n-----\n";
+        cout<<board[0][0]<<" "<<board[0][1]<<" "<<board[0][2]<<" "<<endl;
+        cout<<board[1][0]<<" "<<board[1][1]<<" "<<board[1][2]<<" "<<endl;
+        cout<<board[2][0]<<" "<<board[2][1]<<" "<<board[2][2]<<" "<<endl;
+
+   // for (int r = 0; r<2; r++)
+     // {
+      // for (int c = 0; c<2; c++)
+       // {
+
+for (int i = 0; i<2; i++)
+  {
+   if ((board[i][0]!=BLANK)&&(board[i][1]!=BLANK)&&(board[i][2]!=BLANK)&&(board[i][0]==board[i][1])&&(board[i][0]==board[i][2]))
+     {
+      playing = false;
+      won = true;
+      winner = board[i][0];
+//      cout<<"Win condition 1"<<endl;
+     }
+   if ((board[0][i]!=BLANK)&&(board[1][i]!=BLANK)&&(board[2][i]!=BLANK)&&(board[0][i]==board[1][i])&&(board[0][i]==board[2][i]))
+     {
+      playing = false;
+      won = true;
+      winner = board[0][i];
+//      cout<<"Win Condition 2"<<endl;
+     }  
+  }
+if (board[1][1]!=BLANK&&((board[1][1]==board[0][0]&&board[1][1]==board[2][2])||(board[1][1]==board[0][2]&&board[1][1]==board[2][0])))
+  {
+   playing = false;
+   won = true;
+   winner = board[1][1];
+//   cout<<"Win Condition 3"<<endl;
+  }
   
-    cout<<"\nBOARD\n-----\n";
-for ( int i = 0 ; i <ROWS; i++ )
-{
-	for (int w =0; w<COLUMNS; w++)
-	{
-	cout<<board [i][w]<<" ";
-	}
-	cout<<endl;
-}
-
-
-
-
-
- }while( playing );
-
-  cout<<"Goodbye!\n";
-
+  }while( playing );
+if (won == true)
+  {
+   cout<<"Winner is "<<winner<<endl;
+  }
+   cout<<"Goodbye!\n";
   return 0;
-}
+  }
