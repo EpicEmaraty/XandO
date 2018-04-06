391d434f55d0f36ae1f5b2ae9c395b260e46cb71
EpicEmaraty <35786778+EpicEmaraty@users.noreply.github.com>
2018-04-05 22:28:38 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -58,10 +58,7 @@ int main()
         cout<<board[1][0]<<" "<<board[1][1]<<" "<<board[1][2]<<" "<<endl;
         cout<<board[2][0]<<" "<<board[2][1]<<" "<<board[2][2]<<" "<<endl;
 
-   // for (int r = 0; r<2; r++)
-     // {
-      // for (int c = 0; c<2; c++)
-       // {
+  
 
 for (int i = 0; i<2; i++)
   {
@@ -70,14 +67,14 @@ for (int i = 0; i<2; i++)
       playing = false;
       won = true;
       winner = board[i][0];
-//      cout<<"Win condition 1"<<endl;
+
      }
    if ((board[0][i]!=BLANK)&&(board[1][i]!=BLANK)&&(board[2][i]!=BLANK)&&(board[0][i]==board[1][i])&&(board[0][i]==board[2][i]))
      {
       playing = false;
       won = true;
       winner = board[0][i];
-//      cout<<"Win Condition 2"<<endl;
+
      }  
   }
 if (board[1][1]!=BLANK&&((board[1][1]==board[0][0]&&board[1][1]==board[2][2])||(board[1][1]==board[0][2]&&board[1][1]==board[2][0])))
@@ -85,7 +82,7 @@ if (board[1][1]!=BLANK&&((board[1][1]==board[0][0]&&board[1][1]==board[2][2])||(
    playing = false;
    won = true;
    winner = board[1][1];
-//   cout<<"Win Condition 3"<<endl;
+
   }
   
   }while( playing );
